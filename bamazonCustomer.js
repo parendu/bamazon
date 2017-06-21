517380bdac71a20909fcb944c6e1b8d4da2b86ed
parendu parikh <parendu parikh>
2017-06-21 11:51:58 -0400

    create function purschase with prompt, update database, based on order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,3 +36,74 @@ function startOrder() {
     });
 
 }
+
+
+//create function to prompt for item id and quantity
+function purschase(order) {
+
+    //prompt to purschase item id and quantity
+
+    inquirer.prompt([{
+        type: 'input',
+        name: 'itemId',
+        message: 'Please enter item id, you would like to purchase item: '
+    }, {
+
+        type: 'input',
+        name: 'quantity',
+        message: 'Please enter the quantity, you would like to purchase: '
+    }]).then(function(answer) {
+        console.log("purschase item: " + answer.itemId);
+        console.log("purschase quantity: " + answer.quantity);
+        var itemID = answer.itemId;
+        var quantity = answer.quantity;
+        //find out quantity in database for requested item id
+        connection.query("SELECT stock_quantity, product_name, price from products WHERE ?", { item_id: itemID },
+            function(err, results, fields) {
+                if (!err) {
+
+                    results.forEach(function(element) {
+                        //console.log('Quantity: ', element.stock_quantity);
+                        var current_quantity = element.stock_quantity;
+                        //console.log(current_quantity);
+                        var product_name = element.product_name;
+                        var item_price = element.price;
+                        var Total = itemID * item_price;
+                        //console.log(quantity);
+                        //compare purschase quantity and stock_quantity if stock is not enough, display error message
+                        if (current_quantity > quantity) {
+
+                            var new_quantity = current_quantity - quantity
+                                //if stock is available update the database 
+                            connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: new_quantity }, { item_id: itemID }],
+                                function(err, results) {
+                                    if (err) { console.log(err) };
+                                    console.log("==========Order reciept===========")
+                                    console.log("Your order is completed:");
+                                    console.log("Your ordered item: " + product_name + "  and quantity: " + quantity);
+                                    console.log("Your Total amount is $ " + Total);
+
+                                    console.log("================End================")
+                                    
+                                });
+
+                        } else {
+                            //If stock is not available 
+                            console.log("Insufficient quantity!");
+                            
+                        }
+
+                    }, this);
+
+                } else {
+                    console.log('Error while performing Query.');
+                };
+
+            }); //connection query
+
+
+    }); //function (answer)
+
+
+
+} //purchase function
