37361acb7872cba9fbeea815e9432ff967ec908e
parendu parikh <parendu parikh>
2017-06-21 11:50:01 -0400

    create function startOrder to display products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,3 +19,20 @@ var connection = mysql.createConnection({
 // if(err) throw err;
 // 	console.log("connected as id "+ connection.threadId);
 // });
+
+//function to start new order, display available products
+function startOrder() {
+    connection.query("SELECT * from products", function(err, results, fields) {
+        if (err) { console.log(err) };
+        console.log(results);
+      
+        console.log("item_id | product       | department_name | price | stock_quantity");
+        for (var i = 0; i < results.length; i++) {
+            console.log(results[i].item_id + "     | " + results[i].product_name + " | " + results[i].department_name + " | " + results[i].price + " | " + results[i].stock_quantity)
+
+        
+          };
+       
+    });
+
+}
